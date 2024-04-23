569d026b00f47c8aee048461b2100c2a7d8fdb17
Taufik Herjanto <taufik.herjanto@live.com>
2024-04-23 15:20:55 +0700

    Add delete

--- main.go
+++ main.go
@@ -59,6 +59,21 @@ func main() {
 		return ctx.JSON(http.StatusOK, response)
 	})
 
+	e.DELETE("/todos/:id", func(ctx echo.Context) error {
+		id := ctx.Param("id")
+
+		_, err := db.Exec(
+			"DELETE FROM todos WHERE Id = ?",
+			id,
+		)
+
+		if err != nil {
+			return ctx.String(http.StatusInternalServerError, err.Error())
+		}
+
+		return ctx.String(http.StatusOK, "OK")
+	})
+
 	e.POST("/todos", func(ctx echo.Context) error {
 		var request model.CreateRequest
 		json.NewDecoder(ctx.Request().Body).Decode(&request)
